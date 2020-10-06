7bc14bf86678f7e27ee727212268f54c123e1d69
avilaburgos29 <avilaburgos29@gmail.com>
2020-10-06 01:43:29 -0500

    make it better

---
+++ server/db/index.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    connectionLimit: 10
+    ,password: '1063163660@Emma.'    
+    ,user: 'u427476340_admin'    
+    ,database: 'u427476340_api'
+    ,host: '31.170.161.85'
+    //,port: ''
+});
+
+let pagosdb = {};
+
+pagosdb.all = () => {
+    return new Promise((resolve, reject) =>{
+        pool.query(`SELECT * FROM pagos`, (err, results) =>{
+            if(err){ return reject(err); }
+            return resolve(results);
+        });
+    });
+
+};
+
+pagosdb.one = (id) => {
+    return new Promise((resolve, reject) =>{
+        pool.query(`SELECT * FROM pagos WHERE id = ?`, [id], (err, results) =>{
+            if(err){ return reject(err); }
+            return resolve(results[0]);
+        });
+    });
+
+};
+
+module.exports = pagosdb;
