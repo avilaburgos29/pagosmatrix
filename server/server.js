81f384b33e228fafa155546dc0f7f2b5e6c588ee
avilaburgos29 <avilaburgos29@gmail.com>
2020-10-08 11:24:10 -0500

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const cors = require("cors");
+
 const apiRouter = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(compression());
+app.use(bodyParser.json());
 
 app.use('/api/pagos', apiRouter);
 
